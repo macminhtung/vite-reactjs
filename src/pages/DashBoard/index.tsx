273496f90a97cbb5ddee299ebb00258c46b72c1e
macminhtung <macminhtung1993@gmail.com>
2024-03-28 09:57:17 +0700

    revert code

--- src/pages/DashBoard/index.tsx
+++ src/pages/DashBoard/index.tsx
@@ -1,45 +1,14 @@
-import { memo, useMemo, useState } from 'react';
-import { CustomButton } from 'components/antd';
-// import { useTrans } from 'i18n';
-// import { Link } from 'react-router-dom';
-// import { ROUTER_PATHS } from 'common/constant';
-// import { LanguageKeyEnum } from 'languages';
-
-const TestReactMemo = memo((props: { count: number }) => {
-  return (
-    <div>
-      <p>{new Date().getTime()}</p>
-      <p>Count: {props.count}</p>
-    </div>
-  );
-});
+import { useTrans } from 'i18n';
+import { Link } from 'react-router-dom';
+import { ROUTER_PATHS } from 'common/constant';
+import { LanguageKeyEnum } from 'languages';
 
 export const DashBoard = () => {
-  // const { t } = useTrans();
-  const [count, setCount] = useState(0);
-  const [flag, setFlag] = useState(false);
-
-  const testUseMemo = useMemo(() => {
-    return (
-      <div>
-        <p>{new Date().getTime()}</p>
-        <p>Count: {count}</p>
-      </div>
-    );
-  }, [count]);
+  const { t } = useTrans();
 
   return (
     <div>
-      <CustomButton onClick={() => setCount(count + 1)}>COUNT</CustomButton>
-      <h1>ReactMemo</h1>
-      <TestReactMemo count={count} />
-      <br />
-      <h1>useMemo</h1>
-      {testUseMemo}
-      <br />
-      <CustomButton onClick={() => setFlag(!flag)}>FLAG</CustomButton>
-      <p>Flag: {`${flag}`}</p>
-      {/* <div className='mt-5'>
+      <div className='mt-5'>
         <Link className='text-orange' to={ROUTER_PATHS.SIGNIN}>
           {t(LanguageKeyEnum.SIGNIN)}
         </Link>
@@ -63,7 +32,7 @@ export const DashBoard = () => {
         <Link className='text-orange' to={ROUTER_PATHS.DASHBOARD.WORKER}>
           {t(LanguageKeyEnum.TEST_WORKER)}
         </Link>
-      </div> */}
+      </div>
     </div>
   );
 };
